2f5d4ba0521432d7bb28aec138e97e3c3cf5527c
ismaelnjr <ismaelnjr@icloud.com>
2025-04-25 18:28:11 -0300

    v.0.0.9

--- spedlib/nfe_reader.py
+++ spedlib/nfe_reader.py
@@ -7,12 +7,10 @@ from datetime import date
 from tqdm import tqdm
 from .utils import format_cnpj, format_cpf, list_all_files
 
-
 path = os.path.dirname(os.path.abspath(__file__))
 
 # Carregar layout da NFe
 def load_nfe_layout(json_path):
-    print(os.getcwd())
     with open(json_path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
@@ -22,8 +20,10 @@ def get_columns_by_group(group: list):
     return [(col, config) for col, config in NFE_LAYOUT_CONFIG["columns"].items() if config["group"] in group]
 
 # Funções auxiliares
-def parse_element(element, value_type):
+def parse_element(element, value_type, default_value=None):
     if element is None:
+        if default_value is not None:
+            return default_value
         return "" if value_type == "string" else 0
     try:
         if value_type == "int":
@@ -33,6 +33,8 @@ def parse_element(element, value_type):
         elif value_type == "string":
             return element.text
     except:
+        if default_value is not None:
+            return default_value
         return "" if value_type == "string" else 0
 
 def date_parser(value):
@@ -41,7 +43,7 @@ def date_parser(value):
     return ""
 
 def calculate_total_item(vProd, vFrete, vSeguro, vDesc, vOutros):
-    return vProd + vFrete + vSeguro- vDesc + vOutros
+    return vProd + vFrete + vSeguro - vDesc + vOutros
 
 def calculate_mes_ano(date_str):
     return f"{date_str[-4:]}_{date_str[3:5]}"
@@ -58,34 +60,44 @@ PARSERS = {
     "cpf_parser": format_cpf
 }
 
-# Classe de leitura da NFe
+def find_element_with_multiple_paths(item, config, nsNFe):
+    if config.get("path"):
+        element = item.find(config["path"], nsNFe)
+        if element is not None:
+            return element
+    if config.get("paths"):
+        for path_option in config["paths"]:
+            element = item.find(path_option, nsNFe)
+            if element is not None:
+                return element
+    return None
+
 class NFEReader:
-    
+
     def __init__(self):
         pd.options.display.float_format = "{:,.2f}".format
         self._data = None
-        
+
     @property
     def data(self):
         return self._data
-    
-    def read_nfe(self, filename):
 
+    def read_nfe(self, filename):
         rootXML = ET.parse(filename).getroot()
         nsNFe = {"ns": NFE_LAYOUT_CONFIG["ns"]}
-        
+
         data = []
         if "nfeProc" in rootXML.tag:
             nfe_data = {}
             for col, config in get_columns_by_group(["cabecalho"]):
-                
                 if config.get("atrib"):
-                    value = item.attrib.get(config["atrib"])
-                    item_data[col] = value       
-                elif config.get("path"):
-                    element = rootXML.find(config["path"], nsNFe)
-                    value = parse_element(element, config["type"])
-                    nfe_data[col] = value                    
+                    value = rootXML.attrib.get(config["atrib"])
+                    nfe_data[col] = value
+                elif config.get("path") or config.get("paths"):
+                    element = find_element_with_multiple_paths(rootXML, config, nsNFe)
+                    default_value = config.get("default")
+                    value = parse_element(element, config["type"], default_value)
+                    nfe_data[col] = value
                 elif config.get("function"):
                     args = [nfe_data.get(arg, 0) for arg in config.get("args", [])]
                     nfe_data[col] = FUNCTIONS[config["function"]](*args)
@@ -93,43 +105,41 @@ class NFEReader:
                 if config.get("parser"):
                     nfe_data[col] = PARSERS[config["parser"]](nfe_data[col])
 
-            # Informações dos produtos
             for idx, item in enumerate(rootXML.findall("./ns:NFe/ns:infNFe/ns:det", nsNFe)):
                 item_data = nfe_data.copy()
-                
+
                 for col, config in get_columns_by_group(["item", "imposto", "finalizador"]):
-                    
                     if config.get("atrib"):
                         value = item.attrib.get(config["atrib"])
-                        item_data[col] = value                        
-                    elif config.get("path"):
-                        element = item.find(config["path"], nsNFe)
-                        value = parse_element(element, config["type"])
-                        item_data[col] = value                    
+                        item_data[col] = value
+                    elif config.get("path") or config.get("paths"):
+                        element = find_element_with_multiple_paths(item, config, nsNFe)
+                        default_value = config.get("default")
+                        value = parse_element(element, config["type"], default_value)
+                        item_data[col] = value
                     elif config.get("function"):
                         args = [item_data.get(arg, 0) for arg in config.get("args", [])]
                         item_data[col] = FUNCTIONS[config["function"]](*args)
-                    
+
                     if config.get("parser"):
                         item_data[col] = PARSERS[config["parser"]](item_data[col])
 
-                data.append(item_data)    
+                data.append(item_data)
         return data
 
     def read_all(self, files, verbose=True):
         print("--- Início processamento ---")
         print(f"Total de arquivos encontrados a serem processados: {len(files)}")
-        
+
         data = []
         for xml in tqdm(files, total=len(files), desc="Processando xmls", disable=not verbose):
             for doc in self.read_nfe(xml):
                 data.append(doc)
-        
+
         print("--- Fim processamento ---")
         return data
 
-    
     def read_from_path(self, path, file_ext=".xml"):
         xml_files = list_all_files(path, file_ext)
         self._data = pd.DataFrame(self.read_all(xml_files), columns=NFE_LAYOUT_CONFIG["columns"])
-        return self.data 
+        return self.data
